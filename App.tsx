14fd8fe1b4f67b31cf4087825837a49ed3ca0687
jujulilia <jujuscosta07@gmail.com>
2024-05-02 15:08:59 -0300

    inicial

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './src/screens/Home';
+import PesquisaProduto from './src/screens/PesquisaProduto';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createStackNavigator();
+
+
+function App(): React.JSX.Element {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name ='Home' component={Home} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
---
+++ src/screens/PesquisaProduto.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Head from "../components/Head";
+import Footer from "../components/Footer";
+
+
+
+interface Produto{
+    id:  number;
+    nome: string;
+    ingredientes: string;
+    preco: string;
+    imagem: string;
+}
+
+
+
+function PesquisaProduto(): React.JSX.Element {
+    const produtos: Produto[] = [
+        {
+            id: 1,
+            nome: 'HotDog',
+            ingredientes: 'Pão, batata, carne ...',
+            preco: '18.99',
+            imagem: require('../assets/images/hamburger.png')
+        },
+        {
+            id: 2,
+            nome: 'HotDog Especial',
+            ingredientes: 'Pão, batata, carne ...',
+            preco: '18.99',
+            imagem: require('../assets/images/hamburger.png')
+        },
+        {
+            id: 3,
+            nome: 'HotDog Prensado',
+            ingredientes: 'Pão, batata, carne ...',
+            preco: '18.99',
+            imagem: require('../assets/images/hamburger.png')
+        },
+    
+
+    ]   
+
+    const renderItem = ({item }: {item:Produto}) => {
+        return (
+            <TouchableOpacity style={styles.menuItem}>
+                <Image source={require('../assets/images/hamburger.png')}  style={styles.image}/>
+                <View style={styles.itemDetails}>
+                    <Text style={styles.name}>{item.nome}</Text>
+                    <Text style={styles.description}>{item.ingredientes}</Text>
+                    <Text style={styles.price}>{item.preco}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+
+    return(
+        <View>
+            <StatusBar backgroundColor="red" barStyle="light-content"/>
+            <Head/>
+            <FlatList
+            data={produtos}
+            renderItem={renderItem}
+            keyExtractor={(item)=>item.id ? item.id.toString() : Math.random().toString()}
+            contentContainerStyle={styles.menuList}
+            />
+            <Footer/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    menuItem:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        marginTop: 10
+    },
+    image:{
+        width: 80,
+        height: 80,
+        resizeMode: 'cover',
+        borderRadius: 5
+    },
+    itemDetails:{
+        marginLeft: 10,
+        flex: 1,
+    },
+    name:{
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    description:{
+        fontSize: 14,
+        color: '#666',
+    },
+    price:{
+        fontWeight: 'bold',
+        marginTop: 5
+    },
+    menuList:{
+        flexGrow: 1
+    }
+});
+
+export default PesquisaProduto;
