e1d7c1a286cc957cedb69f7fbd7dd478c59f61ce
jujulilia <jujuscosta07@gmail.com>
2024-05-16 15:44:00 -0300

    Atualização

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import Home from './src/screens/Home';
 import PesquisaProduto from './src/screens/PesquisaProduto';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import Profile from './src/screens/Profile';
+import EditarProduto from './src/screens/EditarProduto';
 
 
 const Stack = createStackNavigator();
@@ -13,7 +15,13 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name ='EditarProduto' component={EditarProduto} options={{ headerShown: false }}/>
+
         <Stack.Screen name ='Home' component={Home} options={{ headerShown: false }}/>
+
+        <Stack.Screen name ='Profile' component={Profile} options={{ headerShown: false }}/>
+
+        <Stack.Screen name ='PesquisaProduto' component={PesquisaProduto} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/PesquisaProduto.tsx
+++ src/screens/PesquisaProduto.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Head from "../components/Head";
 import Footer from "../components/Footer";
+import { useNavigation } from "@react-navigation/native";
 
 
 
@@ -16,6 +17,9 @@ interface Produto{
 
 
 function PesquisaProduto(): React.JSX.Element {
+
+    const navigation = useNavigation();
+
     const produtos: Produto[] = [
         {
             id: 1,
@@ -42,9 +46,15 @@ function PesquisaProduto(): React.JSX.Element {
 
     ]   
 
+    const selecionaProduto = (produto: Produto) => {
+        navigation.navigate('EditarProduto', {produto});
+    }
+
     const renderItem = ({item }: {item:Produto}) => {
         return (
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.menuItem}
+            onPress={()=> selecionaProduto(item)}
+            >
                 <Image source={require('../assets/images/hamburger.png')}  style={styles.image}/>
                 <View style={styles.itemDetails}>
                     <Text style={styles.name}>{item.nome}</Text>
